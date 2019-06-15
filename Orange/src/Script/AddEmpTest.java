a82a9940c1ed53cde8c7338bf98900beaa459873
Welcome <Welcome@LAPTOP-QMLJP549>
2019-06-15 19:01:07 +0530

    Orange 1.1 update

--- Orange/src/Script/AddEmpTest.java
+++ Orange/src/Script/AddEmpTest.java
@@ -1,22 +1,73 @@
 package Script;
 
+import java.awt.AWTException;
+
+import org.openqa.selenium.interactions.Action;
+import org.testng.annotations.Test;
+
 import generics.BaseTest;
 import generics.XL;
 import pages.AddEmpPage;
 import pages.LoginPage;
 
+
 public class AddEmpTest extends BaseTest{
-	
+	@Test
+	public void Addemptest() throws InterruptedException, AWTException {
 	String un=XL.getData(XL_PATH,"Login",1,0);
 	String pw=XL.getData(XL_PATH,"Login",1,1);
 	String fn=XL.getData(XL_PATH, "AddEmp", 1, 0);
 	String ln=XL.getData(XL_PATH, "AddEmp", 1, 1);
 	String id=XL.getData(XL_PATH, "AddEmp", 1, 2);
+	String UN=XL.getData(XL_PATH, "AddEmp", 1, 3);
+	String PW=XL.getData(XL_PATH, "AddEmp", 1, 4);
+	String cpw=XL.getData(XL_PATH, "AddEmp", 1, 5);
 	
-	LoginPage lp=new LoginPage(driver);
+	LoginPage lp;
+	AddEmpPage AEP;
+	lp=new LoginPage(driver);
+	AEP=new AddEmpPage(driver);
+	
+	//enter the username
+	lp.setUN(un);
+	//enter the pwassword
+	lp.setPWD(pw);
+	Thread.sleep(2000);
+	//click on login
+	lp.Loginbtn();
+	Thread.sleep(2000);
+	
+	AEP.clickPIM();
+	Thread.sleep(4000);
+	AEP.clickaddemp();
+	Thread.sleep(4000);
+	//Enter the Firstname
+	AEP.setFN(fn);
+	Thread.sleep(2000);
+	
+	//Enter the LastName
+	AEP.setLN(ln);
+	Thread.sleep(2000);
+	//Enter the Empid
+	AEP.setid(id);
+	Thread.sleep(2000);
+	
+	//Click on checkbox
+	AEP.ClickchkBOX();
+	Thread.sleep(2000);
+	
+	AEP.setUn(UN);
+	Thread.sleep(2000);
+	
+	AEP.setpw(PW);
+	Thread.sleep(2000);
+	
+	AEP.setcpw(cpw);
+	Thread.sleep(2000);
+	//click on save button
+	AEP.Clicksave();
+	
+	
 
-
-	
-	AddEmpPage AEP=new AddEmpPage(driver);
-	
+	}
 }
--- Orange/src/pages/AddEmpPage.java
+++ Orange/src/pages/AddEmpPage.java
@@ -1,7 +1,12 @@
 package pages;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -24,13 +29,37 @@ public class AddEmpPage {
 	@FindBy(id="chkLogin")
 	private WebElement chkBOX;
 	
+	@FindBy(id="user_name")
+	private WebElement UNtxt;
+	
+	@FindBy(id="user_password")
+	private WebElement PWtxt;
+	
+	@FindBy(id="re_password")
+	private WebElement CPWtxt;
+	
 	@FindBy(id="btnSave")
 	private WebElement saveBTN;
-
-	public AddEmpPage(WebDriver driver) {
+public Actions a;
+public Robot r;
+	public AddEmpPage(WebDriver driver) throws AWTException {
 		PageFactory.initElements(driver, this);
+		a=new Actions(driver);
+		  r=new Robot();
 	}
 	
+	public void clickPIM()
+	{
+		pim.click();
+//		 a.moveToElement(pim).build().perform();
+//		 a.moveToElement(pim, 110, 0).build().perform();
+//		 r.mouseMove(110, 0);
+	}
+	
+	public void clickaddemp()
+	{
+		addemp.click();
+	}
 	public void setFN(String fn)
 	{
 		FNtxt.sendKeys(fn);
@@ -49,6 +78,21 @@ public class AddEmpPage {
 	public void ClickchkBOX()
 	{
 	    chkBOX.click();
+	}
+	
+	public void setUn(String un)
+	{
+		UNtxt.sendKeys(un);
+	}
+	
+	public void setpw(String pwd)
+	{
+		PWtxt.sendKeys(pwd);
+	}
+	
+	public void setcpw(String cpw)
+	{
+		CPWtxt.sendKeys(cpw);
 	}
 	
 	public void Clicksave()
